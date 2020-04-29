c94e36b06c0be7f4f357b08bdfbdee92b2e344cf
Jenni Hay <finalfantasyguru8705@gmail.com>
2020-04-29 00:57:39 -0500

    stuff

--- src/com/company/DictionaryPasswordCracker.java
+++ src/com/company/DictionaryPasswordCracker.java
@@ -15,64 +15,63 @@ import java.util.stream.IntStream;
 
 public class DictionaryPasswordCracker {
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, IOException {
-        Path dictionary = Paths.get("PasswordCracking/dic-0294.txt");
 
+        //please change the password filename here...don't include directory.
+        String pwdFilename = "spw1.hex";
+
+
+        //change algorithm because I discovered that the passwords were hashed with different
+        //algorithms.
+        String hashingAlgorithm = "SHA-256";
+
+        Path dictionary = Paths.get("PasswordCracking/dic-0294.txt");
+        Path pw = Paths.get("PasswordCracking/" + pwdFilename);
         Path salt = Paths.get("PasswordCracking/salt.hex");
         FileWriter fileWriter = new FileWriter(new File("results.txt"));
-        String givenSalt = Files.lines(salt).findFirst().get();
-
-for(int x =0; x<=6; x++){
-            for (int i = 1; i <= 3; i++) {
-                Path passwordToCheck = Paths.get("PasswordCracking/pw" + i + ".hex");
-                String pwToValidate = Files.lines(passwordToCheck).findFirst().get();
-                testHash(dictionary, fileWriter, pwToValidate);
-
-            }
-            for (int i = 1; i <= 3; i++) {
-                Path passwordToCheck = Paths.get("PasswordCracking/spw" + i + ".hex");
-                String pwToValidate = Files.lines(passwordToCheck).findFirst().get();
-                testSaltHash(dictionary, fileWriter, givenSalt, pwToValidate);
-            }
+        String lookupHash = Files.lines(pw).findFirst().get();
+        String salt1 = Files.lines(salt).findFirst().get();
+        if (pwdFilename.startsWith("s")) {
+            System.out.println("Testing with salted hash ");
+            testSaltedHash(dictionary, fileWriter, lookupHash, salt1,hashingAlgorithm);
+        } else{
+            testHash(dictionary, fileWriter, lookupHash,hashingAlgorithm);
         }
+
 
 
     }
 
-    private static void testSaltHash(Path dictionary, FileWriter fw, String salt1, String hashToCompare) throws IOException {
+    private static void testHash(Path dictionary, FileWriter fileWriter, String lookupHash, String hashingAlgorithm) throws IOException {
         Map<String, String> map = new HashMap<>();
         for (String text : Files.readAllLines(dictionary)) {
-            map.put(getSecurePassword(salt1, text), text);
+            map.put(getSecurePassword(null, text,hashingAlgorithm), text);
+        }
+        if (map.containsKey(lookupHash)) {
+            System.out.println("THE UNHASHED IS => " + map.get(lookupHash));
+        } else{
+            System.out.println("That hash cannot be found...maybe switch your algorithm!!");
+        }
 
-        }
-        if (map.containsKey(hashToCompare)) {
-            System.out.println("FOUND :: " + map.get(hashToCompare));
-        }
-        writeMapToFile(map, fw);
-        fw.flush();
-        fw.close();
+        fileWriter.write(map + "\n");
+        fileWriter.flush();
+        fileWriter.close();
     }
 
-    private static void testHash(Path dictionary, FileWriter fw, String hashToCompare) throws IOException {
+    private static void testSaltedHash(Path dictionary, FileWriter fileWriter, String lookupHash, String salt1, String hashingAlgorithm) throws IOException {
         Map<String, String> map = new HashMap<>();
         for (String text : Files.readAllLines(dictionary)) {
-            map.put(getSecurePassword(null, text), text);
-
+            map.put(getSecurePassword(salt1, text,hashingAlgorithm), text);
         }
-        if (map.containsKey(hashToCompare)) {
-            System.out.println("FOUND :: " + map.get(hashToCompare));
+        if (map.containsKey(lookupHash)) {
+            System.out.println("THE UNHASHED IS => " + map.get(lookupHash));
         }
-        writeMapToFile(map, fw);
 
+        fileWriter.write(map + "\n");
+        fileWriter.flush();
+        fileWriter.close();
     }
 
-    private static void writeMapToFile(Map<String, String> map, FileWriter fw) throws IOException {
-        fw.write(map + "\n");
-        fw.flush();
-        fw.close();
-
-    }
-
-    //borrowed from online resources
+    //borrowed from stackoverflow
     public static byte[] hexStringToByteArray(String text) {
         byte[] textBytes = new byte[text.length() / 2];
         IntStream.range(0, textBytes.length).forEach(i -> textBytes[i] = (byte) Integer.parseInt(text.substring(2 * i, 2 * i + 2), 16));
@@ -80,13 +79,14 @@ for(int x =0; x<=6; x++){
 
     }
 
-    private static String getSecurePassword(String salt, String passwordToHash) {
+    private static String getSecurePassword(String salt, String passwordToHash, String algorithm) {
         String password = null;
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA");
+            MessageDigest md = MessageDigest.getInstance(algorithm);
             if (salt != null) {
                 md.update(hexStringToByteArray(salt));
             }
+
             byte[] bytes = md.digest(passwordToHash.getBytes());
             StringBuilder sb = new StringBuilder();
             for (byte aByte : bytes) {
